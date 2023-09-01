274b998a4f3d2c69c72a0e0fa7fbb65002f03d18
Mohammad Maroof <mmaroof487@gmail.com>
2023-09-01 20:53:59 +0530

    feat: added cards to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,22 @@
-// Home page should show the loggedIn email ID, and have a button called logout.
 import React, { useState } from "react";
+import Cards from "./Cards";
 
-export default function HomePage({ setIsLoggedIn }) {
+export default function HomePage({ setIsLoggedIn, emailInput, passInput }) {
  const [mouse, setMouse] = useState(false);
  const Info = () => {
+  setMouse((prevMouse) => {
+   return !prevMouse;
+  });
+ };
+ const trueInfo = () => {
   setMouse(true);
  };
- const revInfo = () => {
-  setMouse(false);
+
+ const [eye, setEye] = useState(false);
+ const visibility = () => {
+  setEye((prevEye) => {
+   return !prevEye;
+  });
  };
 
  const logOut = () => {
@@ -20,24 +29,44 @@ export default function HomePage({ setIsLoggedIn }) {
     <div className="text-lg text-white flex  justify-center items-center">
      <span className="material-symbols-outlined">home</span> Home Page
     </div>
-    <div className="text-lg text-white flex  justify-center items-center z-10" onMouseEnter={Info} onMouseLeave={revInfo}>
-     <span className="material-symbols-outlined">person</span> About
-     {mouse ? (
-      <div className="absolute w-60 h-40 right-20 top-12 bg-gray-500 rounded-tr-lg p-2 text-white text-sm shadow-custom">
-       <img src="user.png" alt="user" className="inline-block w-7 mx-2" />
-       About User
-       <div className="my-5 mx-2">User Email</div>
-       <div className="my-5 mx-2 flex  items-center">
-        User password
-        <span className="material-symbols-outlined hidden mx-2">visibility</span>
-        <span className="material-symbols-outlined mx-2">visibility_off</span>
+    <div className="flex flex-row gap-6">
+     <div className="text-lg text-white flex  justify-center items-center">
+      <span class="material-symbols-outlined">shopping_cart</span> Cart
+     </div>
+     <div className="text-lg text-white flex  justify-center items-center z-10" onMouseEnter={Info} onMouseLeave={Info}>
+      <span className="material-symbols-outlined">person</span> About
+      {mouse ? (
+       <div className="absolute w-60 h-40 right-20 top-12 bg-gray-500 rounded-tr-lg p-2 text-white text-sm shadow-custom" onMouseEnter={trueInfo}>
+        <img src="user.png" alt="user" className="inline-block w-7 mx-2" />
+        About User
+        <div className="my-5 mx-2">{emailInput}</div>
+        {eye ? (
+         <div className="flex items-center mx-2" onClick={visibility}>
+          {passInput}
+          <span className="material-symbols-outlined mx-2">visibility</span>
+         </div>
+        ) : (
+         <div className="flex items-center mx-2" onClick={visibility}>
+          User Password
+          <span className="material-symbols-outlined mx-2">visibility_off</span>
+         </div>
+        )}
        </div>
-      </div>
-     ) : (
-      ""
-     )}
+      ) : (
+       ""
+      )}
+     </div>
     </div>
    </nav>
+
+   <main>
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+    <Cards />
+   </main>
 
    <footer>
     <button onClick={logOut} className="absolute bottom-0 right-0 bg-red-600 text-white w-48 h-10 m-3 rounded-sm hover:bg-gray-400 duration-300 ease-in-out flex  justify-center items-center">
